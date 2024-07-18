69c06455ed9fcefe0b089a1050830a2ac112d14e
miyanew <dev38_shin@yahoo.co.jp>
2024-07-18 23:02:22 +0900

    fizzbuzzを実装した

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+function FizzBuzz(max) {
+    let i = 1
+    while (max >= i) {
+        if (i % 15 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i.toString());
+        }
+        i += 1;
+    }
+}
+
+let maxValue = 20;
+FizzBuzz(maxValue);
