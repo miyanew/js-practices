14459e090e883610aadd2a1df15099b17b048ecd
miyanew <dev38_shin@yahoo.co.jp>
2024-07-21 15:40:45 +0900

    Playgroundでコードをフォーマット

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 function fizzBuzz(maxCounter) {
-    for (let i = 1; i <= maxCounter; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('FizzBuzz');
-        } else if (i % 3 === 0) {
-            console.log('Fizz');
-        } else if (i % 5 === 0) {
-            console.log('Buzz');
-        } else {
-            console.log(i.toString());
-        }
+  for (let i = 1; i <= maxCounter; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i.toString());
     }
+  }
 }
 
 const maxCounter = 20;
