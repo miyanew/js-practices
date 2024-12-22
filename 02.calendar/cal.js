f07846fc84d3de3f83db456ea882f170f69a5909
miyanew <dev38_shin@yahoo.co.jp>
2024-12-22 21:17:35 +0900

    表示したい元データを取得する

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,10 +1,25 @@
+#!/usr/bin/env node
+
 import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
-
 const today = new Date();
-const year = argv.y !== undefined ? argv.y : today.getFullYear();
-const month = argv.m !== undefined ? argv.m : today.getMonth() + 1;
+const year = argv.y !== undefined ? parseInt(argv.y) : today.getFullYear();
+const month = argv.m !== undefined ? parseInt(argv.m) : today.getMonth() + 1;
 
-console.log(`-y: ${year}`);
-console.log(`-m: ${month}`);
+const lastDayOfMonth = new Date(year, month, 0);
+
+const monthName = lastDayOfMonth.toLocaleString('en-US', { month: 'long' })
+const weekdays = "Su Mo Tu We Th Fr Sa"
+const days = [];
+for (let day = 1; day <= lastDayOfMonth.getDate(); day++) {
+    days.push(day);
+}
+
+console.log(monthName, year);
+console.log(weekdays);
+console.log(days);
+
+const firstDayOfMonth = new Date(year, month - 1, 1);
+const firstDayOfWeek = firstDayOfMonth.getDay();
+console.log(firstDayOfWeek);
