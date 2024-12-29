04cabc54ead5d05fd07a4ec0bc18b673daf4a519
miyanew <dev38_shin@yahoo.co.jp>
2024-12-29 16:17:58 +0900

    カレンダー表示向けの整形をする

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,24 +2,41 @@
 
 import minimist from "minimist";
 
+// 引数を解析
 const argv = minimist(process.argv.slice(2));
+
 const today = new Date();
 const year = argv.y !== undefined ? parseInt(argv.y) : today.getFullYear();
 const month = argv.m !== undefined ? parseInt(argv.m) : today.getMonth() + 1;
 
 const lastDayOfMonth = new Date(year, month, 0);
 
-const monthName = lastDayOfMonth.toLocaleString('en-US', { month: 'long' })
-const weekdays = "Su Mo Tu We Th Fr Sa"
-const days = [];
-for (let day = 1; day <= lastDayOfMonth.getDate(); day++) {
-    days.push(day);
+// 初週の冒頭の空白埋め
+const firstDayOfMonth = new Date(
+    lastDayOfMonth.getFullYear(),
+    lastDayOfMonth.getMonth(),
+    1,
+);
+const firstDayWeekday = firstDayOfMonth.getDay();
+const initialSpaces = Array(firstDayWeekday).fill(" ".repeat(3));
+
+// 日付ごとの空白埋め + 土曜の改行
+const isSaturday = (weekday) => weekday === 6;
+const days = Array.from({length: lastDayOfMonth.getDate()}, (_, i) => i + 1);
+const formattedDays = days.map((day, index) => {
+  const formattedDay = day.toString().padStart(2, " ");
+  const currentWeekday = (firstDayWeekday + index) % 7;
+  return isSaturday(currentWeekday) ? `${formattedDay}\n` : `${formattedDay} `;
+});
+
+// 最終行は空白行にする
+const lastDayWeekday = lastDayOfMonth.getDay();
+if (!isSaturday(lastDayWeekday)) {
+  formattedDays[formattedDays.length - 1] += "\n";
 }
 
-console.log(monthName, year);
-console.log(weekdays);
-console.log(days);
-
-const firstDayOfMonth = new Date(year, month - 1, 1);
-const firstDayOfWeek = firstDayOfMonth.getDay();
-console.log(firstDayOfWeek);
+// 結果出力
+const monthName = lastDayOfMonth.toLocaleString("en-US", { month: "long" });
+console.log(`   ${monthName} ${year}`);
+console.log("Su Mo Tu We Th Fr Sa");
+console.log([...initialSpaces, ...formattedDays].join(""));
