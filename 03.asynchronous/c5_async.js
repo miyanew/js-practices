9fac0f2da3b9122b5f5d52e0127460700e535be7
miyanew <dev38_shin@yahoo.co.jp>
2025-01-06 23:43:11 +0900

    async版にもヘルパー関数を適用する

--- 03.asynchronous/c5_async.js
+++ 03.asynchronous/c5_async.js
@@ -1,39 +1,28 @@
 import sqlite3 from "sqlite3";
+import { run, get } from "./sqlite_utils.js";
 
 const bookTitles = ["booktitle_01", "booktitle_02", "booktitle_03"];
 const db = new sqlite3.Database(":memory:");
 
 const main = async () => {
-  await new Promise((resolve) => {
-    db.run(
-      `CREATE TABLE books (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL UNIQUE
-       )`,
-      resolve,
-    );
-  });
+  await run(
+    db,
+    `CREATE TABLE books (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL UNIQUE
+    )`,
+  );
 
   for (const title of bookTitles) {
-    await new Promise((resolve) => {
-      db.run("INSERT INTO books (title) VALUES (?)", [title], resolve);
-    });
+    await run(db, "INSERT INTO books (title) VALUES (?)", [title]);
     console.log(`ADD TITLE: ${title}`);
 
-    const book = await new Promise((resolve) => {
-      db.get("SELECT * FROM books WHERE title = ?", [title], (_, book) =>
-        resolve(book),
-      );
-    });
+    const book = await get(db, "SELECT * FROM books WHERE title = ?", [title]);
     console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
   }
 
-  await new Promise((resolve) => {
-    db.run("DROP TABLE books", () => {
-      db.close();
-      resolve();
-    });
-  });
+  await run(db, "DROP TABLE books");
+  db.close();
 };
 
 main();
--- 03.asynchronous/c6_async_contain_error.js
+++ 03.asynchronous/c6_async_contain_error.js
@@ -1,62 +1,40 @@
 import sqlite3 from "sqlite3";
+import { run, get } from "./sqlite_utils.js";
 
 const bookTitles = ["booktitle_01", "booktitle_01", "booktitle_02"];
 const db = new sqlite3.Database(":memory:");
 
 const main = async () => {
-  await new Promise((resolve) => {
-    db.run(
-      `CREATE TABLE books (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL UNIQUE
-       )`,
-      resolve,
-    );
-  });
+  await run(
+    db,
+    `CREATE TABLE books (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL UNIQUE
+    )`,
+  );
 
   for (const title of bookTitles) {
     try {
-      await new Promise((resolve, reject) => {
-        db.run("INSERT INTO books (title) VALUES (?)", [title], (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            console.log(`ADD TITLE: ${title}`);
-            resolve();
-          }
-        });
-      });
+      await run(db, "INSERT INTO books (title) VALUES (?)", [title]);
+      console.log(`ADD TITLE: ${title}`);
     } catch (err) {
       console.error(`ADD TITLE: ${title}, ${err.message}`);
-      continue;
     }
 
     try {
-      await new Promise((resolve, reject) => {
-        db.get(
-          "SELECT * FROM NotExistTable WHERE title = ?",
-          [title],
-          (err, book) => {
-            if (err) {
-              reject(err);
-            } else {
-              console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
-              resolve(book);
-            }
-          },
-        );
-      });
+      const book = await get(
+        db,
+        "SELECT * FROM NotExistTable WHERE title = ?",
+        [title],
+      );
+      console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
     } catch (err) {
       console.error(`GET TITLE: ${title}, ${err.message}`);
     }
   }
 
-  await new Promise((resolve) => {
-    db.run("DROP TABLE books", () => {
-      db.close();
-      resolve();
-    });
-  });
+  await run(db, "DROP TABLE books");
+  db.close();
 };
 
 main();
