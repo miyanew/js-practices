c67ad6a9dbf3660feeea4063f32d4bba9c5e6b4c
miyanew <dev38_shin@yahoo.co.jp>
2025-01-08 00:31:41 +0900

    エラーメッセージを標準エラー出力に出力する

--- 03.asynchronous/c2_callback_contain_error.js
+++ 03.asynchronous/c2_callback_contain_error.js
@@ -1,6 +1,6 @@
 import sqlite3 from "sqlite3";
 
-const bookTitles = ["booktitle_01", "booktitle_02", "booktitle_03"];
+const bookTitles = ["booktitle_01", "booktitle_02", "booktitle_02"];
 const db = new sqlite3.Database(":memory:");
 
 db.run(
@@ -12,11 +12,19 @@ db.run(
     let index = 0;
 
     bookTitles.forEach((title) => {
-      db.run("INSERT INTO books (title) VALUES (?)", [title], () => {
-        console.log(`ADD TITLE: ${title}`);
+      db.run("INSERT INTO books (title) VALUES (?)", [title], (err) => {
+        if (err) {
+          console.error(`ADD TITLE: ${title}, ${err.message}`);
+        } else {
+          console.log(`ADD TITLE: ${title}`);
+        }
 
-        db.get("SELECT * FROM books WHERE title = ?", [title], (_, book) => {
-          console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
+        db.get("SELECT * FROM NotExistTable WHERE title = ?", [title], (err, book) => {
+          if (err) {
+            console.error(`GET TITLE: ${title}, ${err.message}`);
+          } else {
+            console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
+          }
           index++;
 
           if (index >= bookTitles.length) {
