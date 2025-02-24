3b3cce67972f046de8a46ecfd3f7d2756a4a48fc
miyanew <dev38_shin@yahoo.co.jp>
2025-02-24 21:49:54 +0900

    処理内容を統一する

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 import { run, get, close } from "./sqlite_utils.js";
 
-const bookTitles = ["booktitle_01", "booktitle_02", "booktitle_03"];
+const bookTitle = "booktitle_01";
 const db = new sqlite3.Database(":memory:");
 
 await run(
@@ -9,13 +9,13 @@ await run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 );
 
-for (const title of bookTitles) {
-  const result = await run(db, "INSERT INTO books (title) VALUES (?)", [title]);
-  console.log(`ADD TITLE: ${title}, ID: ${result.lastID}`);
+const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
+  bookTitle,
+]);
+console.log(`ADD TITLE: ${bookTitle}, ID: ${result.lastID}`);
 
-  const book = await get(db, "SELECT * FROM books WHERE title = ?", [title]);
-  console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
-}
+const book = await get(db, "SELECT * FROM books WHERE title = ?", [bookTitle]);
+console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
 
 await run(db, "DROP TABLE books");
 
--- 03.asynchronous/async_error.js
+++ 03.asynchronous/async_error.js
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 import { run, get, close } from "./sqlite_utils.js";
 
-const bookTitles = ["booktitle_01", "booktitle_01", "booktitle_02"];
+const bookTitle = "booktitle_01";
 const db = new sqlite3.Database(":memory:");
 
 await run(
@@ -9,35 +9,29 @@ await run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 );
 
-for (const title of bookTitles) {
-  try {
-    const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
-      title,
-    ]);
-    console.log(`ADD TITLE: ${title}, ID: ${result.lastID}`);
-  } catch (err) {
-    if (
-      err instanceof Error &&
-      "code" in err &&
-      err.code === "SQLITE_CONSTRAINT"
-    ) {
-      console.error(`ADD TITLE: ${title}, ${err.message}`);
-    } else {
-      throw err;
-    }
+try {
+  const result = await run(db, "INSERT INTO NotExistTable (title) VALUES (?)", [
+    bookTitle,
+  ]);
+  console.log(`ADD TITLE: ${bookTitle}, ID: ${result.lastID}`);
+} catch (err) {
+  if (err instanceof Error && "code" in err && err.code === "SQLITE_ERROR") {
+    console.error(`ADD TITLE: ${bookTitle}, ${err.message}`);
+  } else {
+    throw err;
   }
+}
 
-  try {
-    const book = await get(db, "SELECT * FROM NotExistTable WHERE title = ?", [
-      title,
-    ]);
-    console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
-  } catch (err) {
-    if (err instanceof Error && "code" in err && err.code === "SQLITE_ERROR") {
-      console.error(`GET TITLE: ${title}, ${err.message}`);
-    } else {
-      throw err;
-    }
+try {
+  const book = await get(db, "SELECT * FROM NotExistTable WHERE title = ?", [
+    bookTitle,
+  ]);
+  console.log(`GET TITLE: ${book.title}, ID: ${book.id}`);
+} catch (err) {
+  if (err instanceof Error && "code" in err && err.code === "SQLITE_ERROR") {
+    console.error(`GET TITLE: ${bookTitle}, ${err.message}`);
+  } else {
+    throw err;
   }
 }
 
